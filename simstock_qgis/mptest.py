ea31c8b79b8d0ac7eb4115bceba843f752069d51
Shyam Amrith <30440839+shyamamrith@users.noreply.github.com>
2022-06-20 17:08:38 +0100

    Standalone script for multiprocessing
    To avoid QGIS spawning multiple windows when using multiprocessing, a separate Python script has been created which can be called by the plugin script using subprocess. This has been tested and works with multiple cores.

---
+++ simstock_qgis/mptest.py
@@ -0,0 +1,38 @@
+import multiprocessing as mp
+#import numpy as np
+#import time
+import os
+import platform
+import subprocess
+
+class EP_Run():
+    def __init__(self):
+        self.plugin_dir = os.path.dirname(__file__)
+        self.EP_DIR = os.path.join(self.plugin_dir, "EnergyPlus")
+        self.idf_dir = os.path.join(self.plugin_dir, "idf_files")
+        self.epw_file = os.path.join(self.plugin_dir, "GBR_ENG_London.Wea.Ctr-St.James.Park.037700_TMYx.2007-2021.epw")
+        files = os.scandir(os.path.abspath(self.idf_dir))
+        self.idf_files = [file.path for file in files if file.name[-4:] == ".idf"]
+        
+        # Find the computer's operating system and find energyplus version
+        system = platform.system().lower()
+        if system in ['windows', 'linux', 'darwin']:
+            self.energyplusexe = os.path.join(self.EP_DIR, 'ep8.9_{}/energyplus'.format(system))
+        
+    def run_ep(self, idf_file):
+        output_dir = idf_file[:-4]
+        subprocess.run([self.energyplusexe, '-r','-d', output_dir, '-w', self.epw_file, idf_file])
+        
+    def run_ep_multi(self):
+        p = mp.Pool()
+        p.map(self.run_ep, self.idf_files)
+        p.close()
+    
+def main():
+
+    #multisquare(cores)
+    runner = EP_Run()
+    runner.run_ep_multi()
+
+if __name__ == '__main__':
+    main()
