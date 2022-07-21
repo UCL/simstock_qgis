105f548e518869905d0e799be8d789b5183316c3
Shyam Amrith <30440839+shyamamrith@users.noreply.github.com>
2022-07-21 13:05:10 +0100

    Reformatted script for readability and editability

--- simstock_qgis/convert_idf_to_database.py
+++ simstock_qgis/convert_idf_to_database.py
@@ -1,13 +1,34 @@
-# %%
 import os
 from eppy.modeleditor import IDF
 import pandas as pd
 import numpy as np
 
-IDF.setiddname(r"EnergyPlus\ep8.9_windows\Energy+.idd")
-idf = IDF("basic_settings backup.idf")
+def main():
+    ### SET ENERGYPLUS IDD FILE:
+    IDF.setiddname(r"EnergyPlus\ep8.9_windows\Energy+.idd")
 
-# %%
+    ### SET THE IDF FILE TO BE CONVERTED:
+    idf = IDF("basic_settings backup.idf")
+
+    ### THE OBJECTS TO BE RETRIEVED FROM THE IDF FILE:
+    # More can be added here if required - the dictonary key name needs to
+    # match the idf object name exactly, except replacing ":" with "_"
+    # and also it is not case-sensitive
+    object_dict = {"material" : idf.idfobjects['MATERIAL'],
+               "material_nomass" : idf.idfobjects['MATERIAL:NOMASS'],
+               "material_infraredtransparent" : idf.idfobjects['MATERIAL:INFRAREDTRANSPARENT'],
+               "material_airgap" : idf.idfobjects['MATERIAL:AIRGAP'],
+               "windowmaterial_glazing" : idf.idfobjects['WINDOWMATERIAL:GLAZING'],
+               "windowmaterial_gas" : idf.idfobjects['WINDOWMATERIAL:GAS'],
+               "construction" : idf.idfobjects['CONSTRUCTION']}
+    
+    ### CONVERT TO CSV FILES:
+    # Note that if the csv files already exist, they will be appended to
+    object_dict = convert_object_dicts(object_dict)
+    dict_to_csv(object_dict)
+
+################################################################################
+
 def to_dict(objects, is_material):
     dicts = []
     for object in objects:
@@ -51,15 +72,3 @@ def dict_to_csv(object_dict):
             if os.path.exists(filename):
                 print("File: '%s' found. Appending to csv..." % filename)
             df.to_csv(filename, mode='a', header=not os.path.exists(filename), index=False)
-
-# %%
-object_dict = {"material" : idf.idfobjects['MATERIAL'],
-               "material_nomass" : idf.idfobjects['MATERIAL:NOMASS'],
-               "material_infraredtransparent" : idf.idfobjects['MATERIAL:INFRAREDTRANSPARENT'],
-               "material_airgap" : idf.idfobjects['MATERIAL:AIRGAP'],
-               "windowmaterial_glazing" : idf.idfobjects['WINDOWMATERIAL:GLAZING'],
-               "windowmaterial_gas" : idf.idfobjects['WINDOWMATERIAL:GAS'],
-               "construction" : idf.idfobjects['CONSTRUCTION']}
-object_dict = convert_object_dicts(object_dict)
-dict_to_csv(object_dict)
-# %%
